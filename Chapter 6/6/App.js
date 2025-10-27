b318120ffb8f3affb35cb4bb20af6657baf25368
Samuel Muvhango <93419130+muvhangos@users.noreply.github.com>
2025-10-27 14:40:02 +0200

    Add files via upload

---
+++ Chapter 6/6/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { Component } from 'react';
+import UserForm from './UserForm';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <UserForm />  
+      </div>
+    );
+  }
+}
+
+export default App;
